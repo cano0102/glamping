c2d635dd03a1d3bcca59354dbb9348abd241da90
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-05-27 23:17:26 -0500

    Implementar clase Alojamiento con atributos, métodos de reserva y cálculo de precios por temporada

---
+++ alojamiento.py
@@ -0,0 +1,63 @@
+from typing import List
+
+class Alojamiento:
+    _numero = 1
+
+    def __init__(self, tipo: str, capacidad_maxima: int, precio_base_noche: float, disponible: bool, amenidades: List[str]):
+        # ATRIBUTOS DE CLASE
+        self.__numero = Alojamiento._numero
+        Alojamiento._numero += 1
+        self.__tipo = tipo
+        self.__capacidad_maxima = capacidad_maxima
+        self.__precio_base_noche = precio_base_noche
+        self.__disponible = disponible
+        self.__amenidades = amenidades
+
+    def mostrar_info(self):
+        return (f"Alojamiento #{self.__numero} - Tipo: {self.__tipo}, "
+                f"Capacidad: {self.__capacidad_maxima}, "
+                f"Precio base: {self.__precio_base_noche}$, "
+                f"Amenidades: {', '.join(self.__amenidades)}, "
+                f"Disponible: {'Sí' if self.__disponible else 'No'}")
+
+    # GETTERS
+    def get_price(self):
+        # GETTER DEL PRECIO DEL ALOJAMIENTO
+        return self.__precio_base_noche
+
+    def get_disponible(self):
+        return self.__disponible
+
+    # SETTERS
+    def set_price(self, nuevo_valor: int):
+        # SETTER DEL PRECIO DEL ALOJAMIENTO
+        self.__precio_base_noche = nuevo_valor
+
+    def set_disponibilidad(self, nuevo_valor: bool):
+        self.__disponible = nuevo_valor
+
+    def calcular_precio_temporada(self, temporada: str):
+        precio = self.get_price()
+        temporada = temporada.capitalize()
+        if temporada == "Alta":
+            self.set_price(precio + (precio * 30) / 100)
+            print(f"Temporada alta se ha aumentado su costo al 30% con un valor final de {self.get_price()}$")
+        elif temporada == "Media":
+            self.set_price(precio + (precio * 10) / 100)
+            print(f"Temporada media se ha aumentado su costo al 10% con un valor final de {self.get_price()}$")
+        else:
+            print(f"Temporada baja con un valor final de {self.get_price()}$")
+        
+    def Reservar(self):
+        if self.get_disponible():  
+            self.set_disponibilidad(False)  
+            print("Reserva hecha correctamente.")
+        else:
+            print("Alojamiento no disponible.")
+
+    def Liberar(self):
+        if not self.get_disponible():  
+            self.set_disponibilidad(True) 
+            print("Reserva liberada correctamente.")
+        else:
+            print("El alojamiento ya está libre.")
