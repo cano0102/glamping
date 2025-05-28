c97ff9851b1dbb0790b2a9d0ee25866214d371e8
cano0102 <canoanderson418@gmail.com>
2025-05-28 00:33:30 -0500

    creacion de la clase servicioAdicional

---
+++ ServicioAdicional.py
@@ -0,0 +1,13 @@
+class ServicioAdicional:
+
+    def __init__(self, nombre, descripcion, precio, duracion_horas):
+        self.__nombre = nombre
+        self.__descripcion = descripcion
+        self.___precio = precio
+        self.__duracion_horas = duracion_horas
+
+    def mostrar_info(self):
+        return( f"Nombre : {self.__nombre}"
+                f"Descripcion: {self.__descripcion}"
+                f"Precio :{self.___precio}"
+                f"Duracion horas: {self.__duracion_horas}")
